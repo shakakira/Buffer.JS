37355fbc9bc2b5de3357182f608f71ceb3d82286
Kayo Phoenix <kayo@illumium.org>
2012-09-30 03:37:56 +0600

    Test suite fixed..

--- test/test.js
+++ test/test.js
@@ -19,18 +19,19 @@ function test(code, asts){
     code = code.join('\n');
   }
   try{
-    run = new Function('ast', code + 'ast(' + asts.join(');ast(') + ');');
+    run = new Function('ast', code + ';ast(' + asts.join(');ast(') + ');');
   }catch(e){
     errors.push('Parsing failed: ' + e + '\n' + code);
-    return;
   }
-  try{
-    run(ast);
-  }catch(e){
-    errors.push('Evaluation failed: ' + e + '\n' + code);
-    return;
+  if(run){
+    try{
+      run(ast);
+    }catch(e){
+      errors.push('Evaluation failed: ' + e + '\n' + code);
+    }
   }
   defer = setTimeout(function(){
-    alert(errors.length ? 'Something went wrong…\n\n' + errors.join('\n\n') : 'All tests passed…');
+    alert(errors.length ? ('Something went wrong...\n\n' + errors.join('\n\n')) :
+          'Used backend: ' + (Buffer.hasDataView ? 'DataView' : 'Object') + '\nAll tests passed...');
   }, 0);
 }
