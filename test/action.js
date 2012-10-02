9875d380c7867278e64a80aaba3358389827fc1b
Kayo Phoenix <kayo@illumium.org>
2012-10-02 10:15:06 +0600

    Concat test added..

--- test/action.js
+++ test/action.js
@@ -26,3 +26,23 @@ test([
   'a.readUInt8(8) == 4',
   'a.readUInt8(9) == 5'
 ]);
+
+test([
+  'var a = Buffer(5)',
+  'a.fill(1)',
+  'var b = Buffer(3)',
+  'b.fill(2)',
+  'var c = Buffer.concat(a, b)',
+  'var d = Buffer.concat([a, b])',
+  'var e = Buffer.concat(b, a, 6)',
+  'var f = Buffer.concat([b, a], 7)'
+], [
+  'c.length == 8',
+  'c.toString("hex") == "0101010101020202"',
+  'd.length == 8',
+  'd.toString("hex") == "0101010101020202"',
+  'e.length == 6',
+  'e.toString("hex") == "020202010101"',
+  'f.length == 7',
+  'f.toString("hex") == "02020201010101"'
+]);
