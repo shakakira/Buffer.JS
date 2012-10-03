6319f01d3ef69bad86ac19cd2f28f7e16c97928d
Kayo Phoenix <kayo@illumium.org>
2012-10-03 10:20:57 +0600

    Feature detection and configuring modernized..

--- buffer.js
+++ buffer.js
@@ -101,6 +101,13 @@
     return buf;
   }
 
+  /* Feature Detecting/Configuring */
+  mix(Buffer, {
+    useArrayBuffer: root.ArrayBuffer && {}.__proto__,
+    useTypedArrays: !!root.Int8Array,
+    useDataView: !!root.DataView
+  });
+
   if(typeof root.Buffer == 'object'){
     mix(Buffer, root.Buffer);
   }
@@ -203,11 +210,7 @@
   write32_ast = cook_ast(4, 3.4028234663852886e+38, -3.4028234663852886e+38, true),
   write64_ast = cook_ast(8, 1.7976931348623157E+308, -1.7976931348623157E+308, true);
 
-  if(root.ArrayBuffer &&
-     root.DataView &&
-     {}.__proto__ &&
-     Buffer.useDataView !== false){
-
+  if(Buffer.useArrayBuffer && Buffer.useDataView){
     var wrap = function(self, start, end){
       // Wrong but ideologically more correct:
       // DataView.call(this, buf)
